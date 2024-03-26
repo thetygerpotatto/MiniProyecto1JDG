18062ecd5bd2d4f1cc2131db210d3599a58d94a0
Gabriel_Uraza611 <137444684+Gabriel-Uraza611@users.noreply.github.com>
2024-03-26 13:44:19 -0500

    Delete Crud.java

--- Crud.java
+++
@@ -1,9 +0,0 @@
-
-//This would be the main class where the crud is being created
-public class Crud 
-{
-    public static void main(String[] args) throws Exception 
-    {
-        System.out.println("Hello, World!");
-    }
-}
