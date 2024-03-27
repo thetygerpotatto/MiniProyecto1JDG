0fafa174714e6873efaffceddad21a050af219d7
thetygerpotatto <letras.cj@gmail.com>
2024-03-27 12:40:54 -0500

    Adding an array list to keep track of clients and a map for keeping track of the money of each client

--- src/Crud.java
+++ src/Crud.java
@@ -1,9 +1,21 @@
+//This would be the main class where the CRUD is being created
 
-//This would be the main class where the crud is being created
+import java.util.Map;
+import java.util.ArrayList;
+
+
 public class Crud 
 {
+
+    static ArrayList<Client> clientes = new ArrayList<Client>();
+    static Map<String, Double> moneyData;
+    
+    static Client cli = null;
+
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Hello, World!");
     }
+
+
 }
