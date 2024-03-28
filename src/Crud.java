5c608feff48e3106c0910f624a7db91b2b57622b
thetygerpotatto <letras.cj@gmail.com>
2024-03-27 23:12:12 -0500

    methods createClient, selectClient, insertMoney and updateMoney are done and listClients and SearchbyName are left to implement

--- src/Crud.java
+++ src/Crud.java
@@ -1,25 +1,25 @@
 //This would be the main class where the CRUD is being created
 
-import java.util.Map;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class Crud 
 {
 
     static ArrayList<Client> clients = new ArrayList<Client>();
-    static Map<String, Double> moneyData = new HashMap<String, Double>() ;
+    static Map<Integer, Double> moneyData = new HashMap<Integer, Double>() ;
     static Client currentClient; 
     
 
     public static void main(String[] args) throws Exception 
     {
-       Menu(); 
+       menu(); 
     }
 
-    static void Menu() 
+    static void menu() 
     {
         int option = -1;
         Scanner scanner = new Scanner(System.in);
@@ -38,7 +38,15 @@ public class Crud
             System.out.println("0- Salir");
 
             option = scanner.nextInt();
-
+            switch (option) {
+                case 1: createClient();  break;
+                case 2: selectClient(); break;
+                case 3: insertMoney(); break;
+                case 4: updateMoney(); break;
+                case 5: searchByName(); break;
+                case 6: listClients(); break;
+                case 0: option = 0;  break;
+            }
         } while (option != 0);
 
         scanner.close();
@@ -62,9 +70,58 @@ public class Crud
         System.out.println("Ingrese la fecha de creacion del ususario:");
         userCreationDate = input.nextInt();
 
+        moneyData.put(idCard, 0.0);
+
         clients.add(new Client(name, idCard, incomeLevel, userCreationDate));
         if (clients.size() == 1) currentClient = clients.get(0);
 
         input.close();
+    } 
+
+    static void selectClient() 
+    {
+        Scanner input = new Scanner(System.in);
+        int index;
+        // the method listClients should show an enumerated list from 1 to the number of users that are in the system;
+        listClients();
+        System.out.println("Ingrese el numero de cliente que desea seleccionar");
+        index = input.nextInt();
+        currentClient = clients.get(index-1);
+        input.close();
+    }
+
+    static void insertMoney() {
+        Scanner input = new Scanner(System.in);
+        double moneyToAdd, moneySaved;
+        moneySaved = moneyData.get(currentClient.getIdCard());
+
+        System.out.println("Ingrese la cantidad que desea agregar a la cuenta");
+        moneyToAdd = input.nextDouble();
+
+        moneyData.put(currentClient.getIdCard(), moneySaved + moneyToAdd);
+
+        input.close();
+    }
+
+    static void updateMoney() {
+        Scanner input = new Scanner(System.in);
+        double money;
+
+        System.out.println("Ingrese la cantidad que desea asignar a la cuenta");
+        money = input.nextDouble();
+
+        moneyData.put(currentClient.getIdCard(), money);
+
+        input.close();
+
+    }
+
+    static void searchByName() {
+       System.out.println("TODO"); 
+    }
+
+    static void listClients()
+    { //TO DO
+       System.out.println("TODO"); 
     }
 }
